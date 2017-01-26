475f76ee19e4269d0cbc3ca56475be80f60d821c
jakobsen9 <jakobsen.marius@gmail.com>
2017-01-26 11:11:22 +0100

    SAML package generating metadata instead of manually.

--- feide/sso.js
+++ feide/sso.js
@@ -2,8 +2,22 @@ var saml = require('express-saml2');
 var express = require('express');
 var router = express.Router();
 
+var spSetting = {
+    entityID:'http://localhost:3000/feide/metadata',
+    assertionConsumerService:[{
+        Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        Location: 'http://localhost:3000/sso/acs'
+    }],
+    singleLogoutService:[{
+        Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+        Location: 'http://localhost:3000/sso/slo'
+    }]
+};
+
 var idp = saml.IdentityProvider('/Users/mariusjakobsen/Desktop/Bachelor-oppgave/feide/metadata/idp.xml');
-var sp = saml.ServiceProvider('/Users/mariusjakobsen/Desktop/Bachelor-oppgave/feide/metadata/sp.xml');
+var sp = saml.ServiceProvider(spSetting);
+
+sp.exportMetadata('/Users/mariusjakobsen/Desktop/Bachelor-oppgave/feide/metadata/sp_md.xml');
 
 router.get('/metadata', function(req, res, next) {
     res.header('Content-Type','text/xml').send(sp.getMetadata());
@@ -16,14 +30,14 @@ router.get('/spinitsso-redirect', function(req, res) {
 });
 
 router.post('/acs', function(req, res, next) {
-    sp.parseLoginResponse(idp, 'post', req, function(parseResult) {
+    sp.parseLoginResponse(idp, 'redirect', req, function(parseResult) {
         // Use the parseResult can do customized action
     res.send('Validate the SAML Response successfully !');
     });
 });
 
 router.post('/slo',function(req, res){
-    sp.parseLogoutRequest(idp, 'post', req, function(parseResult) {
+    sp.parseLogoutRequest(idp, 'redirect', req, function(parseResult) {
         // Check before logout
         req.logout();
         sp.sendLogoutResponse(idp, parseResult, 'redirect', req.body.RelayState, function(url) {

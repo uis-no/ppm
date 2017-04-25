b04c3bf5f0eb0c810533e94afec842002341d2ab
Brynjulf <brynjulfz@hotmail.com>
2017-04-26 00:30:38 +0200

    removed unused file

--- database.ts
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://admin:admin@ds061196.mlab.com:61196/ppm');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', console.log('connection running'));

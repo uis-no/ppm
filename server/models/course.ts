3121b686b4df7f48a7ef349ee18d80a2b78e121e
Brynjulf <brynjulfz@hotmail.com>
2017-05-04 14:06:08 +0200

    added deadline attribute to course models, both client and server

--- server/models/course.ts
+++ server/models/course.ts
@@ -3,13 +3,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var course = new Schema({
-  
+
   _id : String,
 
   course : String,
 
-  year : Number
+  year : Number,
 
+  deadlines: {
+    instituteSuggest: Date,
+    studentSuggest: Date,
+    apply: Date,
+    accept: Date,
+    start: Date,
+    submit: Date
+  }
 });
 
 
